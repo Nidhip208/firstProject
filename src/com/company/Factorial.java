5a7177a9b738c56ec9dc540d63ca1a1d678434f1
nidhip208 <nidhip208@gmail.com>
2022-11-01 21:02:30 -0400

    This is a Lucky No.

---
+++ src/com/company/Factorial.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=5;i++){
+            int d=n;
+            System.out.println(n=n*(d-1));
+             d=(n-1);
+
+
+        }
+    }
+}
