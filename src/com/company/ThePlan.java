a78a816e1b4964157537f5f792d7c8f04701cbdb
nidhip208 <nidhip208@gmail.com>
2022-11-07 21:40:02 -0500

    changes

--- src/com/company/ThePlan.java
+++ src/com/company/ThePlan.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class gi {
+public class ThePlan {
 
 
     public static void main(String[] variables) {
@@ -8,6 +8,7 @@ public class gi {
         };
          for (int i = 0; i < days.length; i++)
             System.out.println(days[i]);
+        System.out.println("These are new changes");
 
     }
 }
